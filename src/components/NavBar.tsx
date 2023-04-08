62e56fc631c14e4b8d4b1fab54c74af07f4a4f46
Sioulis Nick <sioulisn@gmail.com>
2023-04-08 11:34:00 +0300

    Add a dynamic page heading

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,14 +1,18 @@
-import { HStack, Image } from "@chakra-ui/react";
+import { HStack, Image, Link } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
+
 interface Props {
   onSearch: (searchText: string) => void;
 }
+
 function NavBar({ onSearch }: Props) {
   return (
     <HStack padding="10px">
-      <Image src={logo} boxSize="60px" />
+      <Link boxSize="60px" href="/">
+        <Image src={logo} />
+      </Link>
       <SearchInput onSearch={onSearch} />
       <ColorModeSwitch />
     </HStack>
