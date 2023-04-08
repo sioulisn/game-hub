19193f40fde91edd60c95e4dc17976e101144450
Sioulis Nick <sioulisn@gmail.com>
2023-04-08 12:43:22 +0300

    Ship genres with app + Ship platforms with app

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,10 +1,10 @@
 import platforms from "../data/platforms";
-
-interface Platform {
-  id: number;
-  name: string;
-  slug: string;
-}
+//
+// interface Platform {
+//   id: number;
+//   name: string;
+//   slug: string;
+// }
 
 const usePlatforms = () => ({ data: platforms, isLoading: false, error: null });
 //const usePlatforms = () => useData<Platform>("/platforms/lists/parents");
